1a0f68e106e3ba14538953acba79d1b6076bc3d5
MamoonRazmal <41575028+MamoonRazmal@users.noreply.github.com>
2025-01-16 11:02:50 +0100

    newch

--- Netfy/StateStore/ChatHub.cs
+++ Netfy/StateStore/ChatHub.cs
@@ -1,10 +1,27 @@
 using Microsoft.AspNetCore.SignalR;
+using Netfy.Components.Model;
 public class ChatHub: Hub
 {
+    private static readonly List<User> LoggedInUsers =new();
+
+public async Task addUser(User user)
+{
+    if(!LoggedInUsers.Any(u=>u.username ==user.username))
+    {
+         LoggedInUsers.Add(user);
+         await Clients.All.SendAsync("RecievedList",LoggedInUsers);
+    }
+   
+}
+
 public async Task sendMessage(string user,string message)
 {
     await Clients.All.SendAsync("ReceiveMessage",user,message);
 
+}
+public async Task sendUserList(List<User> userlist)
+{
+    await Clients.All.SendAsync("RecievedListq",userlist);
 }
 
 }
